3395e2b50caf59f77062427f725a281665d641c3
unknown <coolgood88142@gmail.com>
2019-12-09 03:31:46 +0800

    新增縣市選擇器使用Component(未完成:vue的component功能未完成)

---
+++ component/test.vue
@@ -0,0 +1,17 @@
+
+<template lang="html">
+    <select name = "counties">
+        <option v-for="todo in todos">
+            {{ todo.text }}
+        </option>
+    </select>
+</template>
+
+<script>
+import testComponent from 'test-component'
+export default {
+    components: {
+        testComponent 
+    }
+}
+</script>
---
+++ js/compement.js
@@ -0,0 +1,11 @@
+let counties = [
+    {text : '台北市', value : 0},
+    {text : '基隆市', value : 1}
+  ];
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        todos: counties
+    }
+})
