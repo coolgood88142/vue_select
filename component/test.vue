80bfc113ce5940fd492c2fad5a73c9337d9bcdba
Kai <coolgood88142@gmail.com>
2019-12-10 04:03:10 +0800

    新增縣市選擇器使用Component(未完成:vue的component功能未完成)

--- component/test.vue
+++ component/test.vue
@@ -1,17 +1,11 @@
 
-<template lang="html">
-    <select name = "counties">
-        <option v-for="todo in todos">
-            {{ todo.text }}
-        </option>
-    </select>
-</template>
-
 <script>
 import testComponent from 'test-component'
+
 export default {
     components: {
         testComponent 
-    }
+    },
+    template:'<select name = "counties"><option v-for="todo in todos">{{ todo.text }}</option></select>'
 }
 </script>
